46a6387fbfee3add58b76b3a1755453ebd637b79
rwilt <rosie.wilt@gmail.com>
2020-04-22 10:48:52 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,16 +1,5 @@
 
-# class Person
-
-#     attr_accessor :age
-  
-#     def initialize(age = 0)
-#       @age = age 
-#     end 
-  
-#     def birthday
-#       self.age += 1
-#     end
-#   end
+#solved, then re-wrote because i didnt  use "self" at all at first. 
 
 class CashRegister
     attr_accessor :total
@@ -29,14 +18,14 @@ class CashRegister
         
           if quantity
             #using concat adds the item n number of times
-            @items.concat([name]*quantity)
+            self.items.concat([name]*quantity)
             price = (price * quantity)
             @last_transaction = price
             @total += price
         else 
-            @items.push(name)
+            self.items.push(name)
            @last_transaction = price
-          @total += price
+           self.total += price
         end 
     end
 
@@ -50,10 +39,10 @@ class CashRegister
     end
 
     def apply_discount
-        if @discount != 0
-        @total = @total * (100 - @discount) / 100
-        return  "After the discount, the total comes to $#{@total.to_i}."
-        elsif @discount == 0
+        if self.discount != 0
+        self.total = @total * (100 - @discount) / 100
+        return  "After the discount, the total comes to $#{self.total.to_i}."
+        elsif self.discount == 0
         return "There is no discount to apply."
         end
     end
@@ -63,7 +52,7 @@ class CashRegister
 #it becomes the last transaction
 
 def void_last_transaction
-@total = @total - @last_transaction
+self.total = @total - @last_transaction
 
 end
 
